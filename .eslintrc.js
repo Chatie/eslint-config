9a7d8a6a87006c1c414244aaea6601f61617ad9e
Huan LI <zixia@zixia.net>
2019-06-12 00:51:07 +0800

    fix unused vars false alarm https://github.com/typescript-eslint/typescript-eslint/issues/379

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,5 +49,8 @@ module.exports =  {
     'key-spacing': 'off',
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
     'no-multi-spaces': 'off',
+    // note you must disable the base rule as it can report incorrect errors
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
 }
