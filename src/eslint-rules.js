756e33963c864c2f550b29eff124af2e6130bfb0
Huan LI <zixia@zixia.net>
2019-06-19 02:02:24 +0800

    add require-await

--- src/eslint-rules.js
+++ src/eslint-rules.js
@@ -1,16 +1,16 @@
 const ESLINT_RULES = {
+  '@typescript-eslint/no-unused-vars': 'error',
+  '@typescript-eslint/no-useless-constructor': 'off',
   'comma-dangle': ['error', 'always-multiline'],
   'key-spacing': 'off',
   'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+  'no-dupe-class-members': 'off',
   'no-multi-spaces': 'off',
   'no-unused-vars': 'off',
-  '@typescript-eslint/no-unused-vars': 'error',
   'no-useless-constructor': 'off',
-  '@typescript-eslint/no-useless-constructor': 'off',
-  'no-dupe-class-members': 'off',
   'operator-linebreak': ['error', 'before'],
   'padded-blocks': ['error', { 'classes': 'always' }],
-
+  'require-await': 'error',
 }
 
 module.exports = {
